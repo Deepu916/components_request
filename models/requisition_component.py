e719d7c9409cfce3859a883c225e360f582434d2
Deepu916 <deepuenas999@gmail.com>
2026-05-12 17:37:39 +0530

    Add files via upload

--- models/requisition_component.py
+++ models/requisition_component.py
@@ -4,6 +4,7 @@ from odoo.exceptions import ValidationError
 
 
 class RequisitionComponent(models.Model):
+    """Requisition Component class"""
     _name = 'requisition.component'
     _description = 'Requisition Component'
     _rec_name = 'employee_id'
@@ -13,6 +14,12 @@ class RequisitionComponent(models.Model):
     line_ids = fields.One2many('requisition.line', 'requisition_id', string='Lines',ondelete='cascade')
     create_po = fields.Boolean(default=False)
     create_internal_transfer = fields.Boolean(default=False)
+    po_count = fields.Integer(default=0)
+    po_smart = fields.Boolean(default=False)
+    in_smart = fields.Boolean(default=False)
+    internal_count = fields.Integer(default=0)
+    order_ids = []
+    internal_ids = []
     state = fields.Selection([
         ('draft', 'Draft'),
         ('requested', 'Requested'),
@@ -20,8 +27,8 @@ class RequisitionComponent(models.Model):
         ('head','Head Approved'),
         ('rejected','Rejected'),
     ],default='draft')
-
     def action_request(self):
+        """Request button action"""
         if not self.line_ids:
             raise ValidationError('Product line is empty')
         for product in self.line_ids:
@@ -33,10 +40,13 @@ class RequisitionComponent(models.Model):
                 self.create_internal_transfer = True
         self.state = 'requested'
     def action_manager(self):
+        """Manager approval action"""
         self.state = 'manager'
     def action_head(self):
+        """Head approval action"""
         self.state = 'head'
     def action_create_po(self):
+        """"Create PO action"""
         if self.state!='head':
             raise ValidationError('PO can create only after the approval of the head')
         return{
@@ -45,24 +55,53 @@ class RequisitionComponent(models.Model):
             'res_model': 'create.po',
             'view_mode': 'form',
             'target': 'new',
-            'context': {'default_line_ids': self.line_ids.ids},
+            'context': {'default_line_ids': self.line_ids.ids,'default_create_type':'po','default_parent_id': self.id},
         }
-    # def action_create_in(self):
-    #     return {
-    #         'name':'Create Internal Transfer',
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'create.po',
-    #         'view_mode': 'form',
-    #         'target': 'new',
-    #         'context': {'create_type':'internal'}
-    #     }
+    def action_create_in(self):
+        """Create Internal Transfer action"""
+        if self.state!='head':
+            raise ValidationError('PO can create only after the approval of the head')
+        return {
+            'name':'Create Internal Transfer',
+            'type': 'ir.actions.act_window',
+            'res_model': 'create.po',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {'default_line_ids': self.line_ids.ids,'default_create_type':'in'}
+        }
     def action_reject(self):
+        """Reject action"""
         self.state = 'rejected'
-
+    # def action_po(self):
+    #     """PO smart button action"""
+    #     print(self.order_ids)
+    #     return{
+    #         'name':'Purchase Orders',
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'purchase.order',
+    #         'view_mode': 'list,form',
+    #         'domain':[('id', 'in', self.order_ids)],
+    #     }
+    # def action_in(self):
+    #     """Internal Transfer smart button action"""
+    #     return{
+    #         'name':'Internal Transfers',
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'stock.picking',
+    #         'view_mode': 'list,form',
+    #         'domain':[('id', 'in', self.internal_ids),('')],
+    #     }
 class RequisitionLine(models.Model):
+    """Requisition line model"""
     _name = 'requisition.line'
     _description = 'Requisition Line'
 
     requisition_id = fields.Many2one('requisition.component', string='Requisition')
     product_id = fields.Many2one('product.product', string='Product')
-    quantity = fields.Float(string='Quantity', default=0.0)
+    quantity = fields.Float(string='Quantity', default=0.0,required=True)
+    uom_id = fields.Many2one('uom.uom', related='product_id.uom_id', string='Units')
+    po = fields.Boolean(default=False)
+    internal = fields.Boolean(default=False)
+    qty_on_hand = fields.Float(string='Qty On Hand',related = 'product_id.qty_available')
+    source_id = fields.Many2one('stock.location',related='product_id.location_id',string='Source')
+    destination_id = fields.Many2one('stock.location', string='Destination Location')
--- wizard/create_po.py
+++ wizard/create_po.py
@@ -1,33 +1,69 @@
 # -*- coding: utf-8 -*-
-from email.policy import default
-
 from odoo import fields, models, api
 
 
 class CreatePo(models.TransientModel):
+    """Create Po Wizard"""
     _name = 'create.po'
     _description = 'Create Po'
 
-    create_type = fields.Selection([
-        ('po', 'PO'),
-        ('in','IN')
-    ],default=lambda self:self.env.context.get('create_type'))
-    partner_id = fields.Many2many('res.partner',string='Vendors',required=True)
+
+    partner_ids = fields.Many2many('res.partner',string='Vendors',required=True)
     line_ids = fields.Many2many('requisition.line',ondelete='cascade',required=True)
-    quantity = fields.Float(string='Quantity',required=True)
-    creation_type = fields.Char(string='Creation Type',required=True,default=lambda self:self.env.contex.get('creation_type'))
+    destination_location_id = fields.Many2one('stock.location',string='Destination Location')
+    create_type = fields.Char(string='Create Type')
+    parent_id = fields.Many2one('requisition.component')
+    orders = []
+    internal = []
+
+
     def action_create_purchase_order(self):
-        for ids in self.partner_id:
+        """Create Purchase Order action"""
+        self.parent_id.order_ids.clear()
+        for ids in self.partner_ids:
             order = self.env['purchase.order'].create(
                 {
                     'partner_id':ids.id,
                 }
             )
+            self.parent_id.order_ids.append(order.id)
+            self.orders.append(order.id)
             for line in self.line_ids:
-                self.env['purchase.order.line'].create({
-                    'order_id': order.id,
-                    'product_id': line.product_id.id,
-                    'product_qty':line.quantity,
-                })
-    # def action_create_internal_transfer(self):
+                if line.qty_on_hand < line.quantity:
+                    self.env['purchase.order.line'].create({
+                        'order_id': order.id,
+                        'product_id': line.product_id.id,
+                        'product_qty': line.quantity,
+                    })
+        self.parent_id.po_count = len(self.orders)
+        self.parent_id.create_po = False
+        self.parent_id.po_smart = True
+        print(len(self.partner_ids))
 
+    def action_create_internal_transfer(self):
+        """create internal transfer action"""
+        picking_type_id = self.env['stock.picking.type'].search([('name','=','Internal Transfers')])
+        print("picking type",picking_type_id)
+
+        for line in self.line_ids:
+            if line.qty_on_hand > line.quantity:
+                quant = self.env['stock.quant'].search([('product_id','=',line.product_id)],limit=1)
+                stock = self.env['stock.picking'].create({
+                    'picking_type_id': picking_type_id.id,
+                    'partner_id':self.env.user.employee_id.id,
+                    'location_id':quant.location_id.id,
+                    'location_dest_id':self.destination_location_id.id,
+                })
+                self.internal.append(stock.id)
+                self.parent_id.internal_ids.append(stock.id)
+                self.env['stock.move'].create({
+                    'picking_id':stock.id,
+                    'product_id':line.product_id.id,
+                    'product_uom_qty':line.quantity,
+                })
+        print(len(self.internal))
+        self.parent_id.internal_count = len(self.internal)
+        self.parent_id.create_internal_transfer = False
+        self.parent_id.in_smart = True
+        stock.button_validate()
+        print(self.parent_id.create_internal_transfer)
